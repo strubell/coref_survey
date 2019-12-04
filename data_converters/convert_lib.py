003a202217760090670db04b7461f4c4af07893a
Emma Strubell <strubell@fb.com>
2019-12-04 15:25:47 -0500

    debug

--- data_converters/convert_lib.py
+++ data_converters/convert_lib.py
@@ -247,12 +247,14 @@ class Document(object):
 
     # update clusters to index into tokenized
     for max_len, tokenized_sents in self.tokenized_sentences.items():
-      for idx, (this_toks, this_subtoks_map), in enumerate(zip(tokenized_sents.segments, tokenized_sents.subtoken_map)):
-        this_clusters = self.clusters[idx]
-        print("Document {}".format(idx))
-        print(this_toks)
-        print(this_subtoks_map)
-        print(this_clusters)
+      print(len(tokenized_sents.segments))
+      print(len(tokenized_sents.subtoken_map))
+      # for idx, (this_toks, this_subtoks_map), in enumerate(zip(tokenized_sents.segments, tokenized_sents.subtoken_map)):
+      #   this_clusters = self.clusters[idx]
+      #   print("Document {}".format(idx))
+      #   print(this_toks)
+      #   print(this_subtoks_map)
+      #   print(this_clusters)
 
   def apply_dump_fn(self, function):
     return self.FN_MAP[function]()
